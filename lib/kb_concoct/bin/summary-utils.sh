52001903bd55bb33876c9f48f507d29dd0636073
jungbluth <jungbluth.sean@gmail.com>
2019-11-05 11:33:24 -0800

    Added fix to generate summary file

--- lib/kb_concoct/bin/summary-utils.sh
+++ lib/kb_concoct/bin/summary-utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+echo "Running summary-utils.sh script"
 # set defaults
 BINS='final_bins'
 
@@ -27,9 +27,9 @@ set -x
 # I'm creating a fake one.  The binned contigs object needs to be updated
 # so this is not necessary.
 echo -e "Bin name\tCompleteness\tGenome size\tGC content" > $BINS/notreal.summary
-testRun "create $BINS/notreal.summary" $? 
+testRun "create $BINS/notreal.summary" $?
 
-# Unfortunately, the MetagenomeUtils function that loads the bins into "binned_contig_obj_ref" 
+# Unfortunately, the MetagenomeUtils function that loads the bins into "binned_contig_obj_ref"
 # uses a regular expression that requires the bin names to be in the form <something>.123.fasta
 # So I'm converting bin names here.
 
@@ -45,14 +45,14 @@ for i in ${BINSARRAY[@]}; do
       newID="unbinned"
     else
       COUNT=$((COUNT+1))
-      binID=$(basename $i) 
+      binID=$(basename $i)
       binID=${binID%.fa}
       binID=${binID#bin.}
       newID=$(printf "%03d\n" $binID)
     fi
-    
+
     mv $i $BINS/bin.$newID.fasta
-    
+
 	# lets populate the .summary file (this is a work around until MetagenomeUtils gets fixed)
 	# The collumns are binID, read coverage, Total Contig Length, GC%
     echo -e "bin.$newID.fasta\t0\t0\t0" >> $BINS/notreal.summary
@@ -60,4 +60,3 @@ done
 
 # cleanup large files
 rm -r $BINSDIR
-
