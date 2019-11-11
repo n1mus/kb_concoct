cc9b89dfa913f384c415d7300765a083171e1af9
jungbluth <jungbluth.sean@gmail.com>
2019-11-11 13:45:47 -0800

    Hack to get new parameters working through UI

--- lib/kb_concoct/Utils/ConcoctUtil.py
+++ lib/kb_concoct/Utils/ConcoctUtil.py
@@ -238,14 +238,14 @@ class ConcoctUtil:
             parameter_no_total_coverage = '--no_total_coverage'
         elif params.get('no_total_coverage') is 0:
             parameter_no_total_coverage = ' '
-        else
+        else:
             continue
 
         if params.get('parameter_no_cov_normalization') is 1:
             parameter_no_cov_normalization = '--parameter_no_cov_normalization'
         elif params.get('parameter_no_cov_normalization') is 0:
             parameter_no_cov_normalization = ' '
-        else
+        else:
             continue
 
 
