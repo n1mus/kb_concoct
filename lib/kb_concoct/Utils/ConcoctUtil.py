8f6f7164a5f6959c398f78142124fcf2b60af050
jungbluth <jungbluth.sean@gmail.com>
2019-11-11 12:59:48 -0800

    Hack to get new parameters working through UI

--- lib/kb_concoct/Utils/ConcoctUtil.py
+++ lib/kb_concoct/Utils/ConcoctUtil.py
@@ -233,6 +233,17 @@ class ConcoctUtil:
         generate_command: generate concoct params
         """
 
+        # needed to get checkbox for UI to work with string objects
+        if params.get('no_total_coverage') is 1:
+            parameter_no_total_coverage = '--no_total_coverage'
+        elif params.get('no_total_coverage') is 0:
+            parameter_no_total_coverage = ' '
+
+        if params.get('parameter_no_cov_normalization') is 1:
+            parameter_no_total_coverage = '--parameter_no_cov_normalization'
+        elif params.get('parameter_no_cov_normalization') is 0:
+            parameter_no_total_coverage = ' '
+
         print("\n\nRunning generate_concoct_command")
 
         print("no_cov_normalization is: " + str(params.get('no_cov_normalization')))
@@ -242,7 +253,7 @@ class ConcoctUtil:
         command += 'python /kb/deployment/bin/CONCOCT/scripts/concoct_coverage_table.py temp.bed concoct_output_dir/*.sorted.bam > concoct_output_dir/coverage_table.tsv'
         command += ' && '
         command += 'python /kb/deployment/bin/CONCOCT/bin/concoct --composition_file concoct_output_dir/split_contigs.fa -l {} -b concoct_output_dir --coverage_file concoct_output_dir/coverage_table.tsv -t {} -k {} -c {} -i {} --total_percentage_pca {} {} {}'.format(params.get('min_contig_length'),
-                                         self.BBMAP_THREADS,params.get('kmer_size'),params.get('max_clusters_for_vgmm'),params.get('max_iterations_for_vgmm'),params.get('total_percentage_pca'),params.get('no_cov_normalization'),params.get('no_total_coverage'))
+                                         self.BBMAP_THREADS,params.get('kmer_size'),params.get('max_clusters_for_vgmm'),params.get('max_iterations_for_vgmm'),params.get('total_percentage_pca'),parameter_no_cov_normalization,parameter_no_total_coverage)
         command += ' && '
         command += 'python /kb/deployment/bin/CONCOCT/scripts/merge_cutup_clustering.py concoct_output_dir/clustering_gt{}.csv > concoct_output_dir/clustering_merged.csv'.format(params.get('min_contig_length'))
         command += ' && '
